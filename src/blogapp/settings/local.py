2739e05f855f4bea107d2386192b5e8bd4efa0c6
Frank UN <frankuchenna30@gmail.com>
2020-09-20 15:26:01 +0100

    updated settings config

--- src/blogapp/settings/local.py
+++ src/blogapp/settings/local.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'accounts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
